ecad0db497c2a9c9a50d7cf5001b132070bc6049
Luís Felipe Silva Ferreira <152612333+IamLiper@users.noreply.github.com>
2025-02-20 14:30:42 +0000

    novo desafio adicionado

---
+++ Desafios/desafio004.py
@@ -0,0 +1,9 @@
+algo = input("Digite algo: ")
+
+print(type(algo))
+print(algo.isalpha())
+print(algo.isnumeric())
+print(algo.isalnum())
+print(algo.isdecimal())
+print(algo.isupper())
+print(algo.islower())
