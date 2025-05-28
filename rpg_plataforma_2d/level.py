0c76bbe593951d8936818a824a1c9166c8bbe3d8
Luís Felipe Silva Ferreira <152612333+IamLiper@users.noreply.github.com>
2025-05-28 16:35:13 -0300

    Delete rpg_plataforma_2d directory

--- rpg_plataforma_2d/level.py
+++
@@ -1,7 +0,0 @@
-# level.py
-import pygame
-from config import *
-
-def draw_ground(surface):
-    ground_rect = pygame.Rect(0, SCREEN_HEIGHT - GROUND_HEIGHT, SCREEN_WIDTH, GROUND_HEIGHT)
-    pygame.draw.rect(surface, GROUND_COLOR, ground_rect)
